21fd262a4cea8fd7b824d9e1ddc1f5653c091954
Avishek Kumar <avishek.kumar@asu.edu>
2016-02-21 15:32:28 -0700

    Updated first test case

--- dfitools.py
+++ dfitools.py
@@ -9,13 +9,16 @@ Suite of tools for DFI Analysis
 
 """
 
-def make_fafsta(name,df_dfi):
+def make_fasta(name,df_dfi):
     """
     name: name of fasta index
     df_dfi: df of dfi profile
     must have sequence under 
     column R 
     """
+    import textwrap
     seq = ''.join(df_dfi.R.values)
-    print ">%s"%name
-    print seq
+    nameline=">%s\n"%name
+    seqline = textwrap.fill(seq,80)
+    return nameline+seqline 
+    
--- testdfitools.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python 
-
-def test_make_fafsta():
-    pass 
