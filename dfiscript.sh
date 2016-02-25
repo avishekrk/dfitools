a19edfb18e1ace19852eb8a96a303cf6f0ca12a5
- <avishek.kumar@asu.edu>
2016-02-25 13:19:44 -0600

    Updated the dfiscript

--- dfiscript.sh
+++ dfiscript.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 #small script to run time series DFI 
 
+if [ -z "${1}" ]
+then
+	echo "No input file";
+	exit;
+fi
+
+pdbid=$1
+file=${pdbid}.pdb
+if [ -f "${file}" ]
+then
+	echo "Found the file $file"
+else
+	echo "Did not find the $file"
+	exit 1 
+
+fi 
 
 for f in *_mwcovarmat.dat
 do
     echo $f;
     prefix=$(echo $f | sed "s/_mwcovarmat.dat//g")
     echo $prefix
-    ~/dfi/dfi.py --pdb bpti.pdb --hess $f
-    mv -v bpti-dfianalysis.csv ${prefix}-dfianalysis.csv 
+    ~/dfi/dfi.py --pdb ${pdbid}.pdb --hess $f
+    mv -v ${pdbid}-dfianalysis.csv ${prefix}-dfianalysis.csv 
 
 done
