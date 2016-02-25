f360c2cf3aa466704d385a1abfb5d183f86b8c5d
Avishek Kumar <avishek.kumar@asu.edu>
2016-02-25 16:35:07 -0700

    added the globbing and adding functions to dfitools

--- dfitools.py
+++ dfitools.py
@@ -22,3 +22,30 @@ def make_fasta(name,df_dfi):
     seqline = textwrap.fill(seq,80)
     return nameline+seqline 
     
+def glob_df(pdbid,parm,chainA=False):
+    "Take a bunch of dataframe and combine them"
+    csvfiles = glob.glob(pdbid+'*.csv')
+    titles = map(lambda x: x.replace(pdbid+'_','').replace('-dfianalysis.csv',''),csvfiles)
+    data = pd.concat( [pd.read_csv(fname, index_col='ResI') for fname in csvfiles], keys=titles, names=['window','ResI'])
+    if chainA:
+        data = data[data.ChainID=='A']
+    df_windows = pd.DataFrame()
+    for win, df_dfi in data.groupby(level=0):
+        df_windows[win] = df_dfi.reset_index().set_index('ResI')[parm]
+    return df_windows 
+
+def getavg(df_windows):
+    "get the average of all frames execpt ENM"
+    df_windows = glob_df(pdbid,'pctdfi')
+    cols = [col for col in df_windows.columns if not(col == "ENM")]
+    df_windows['md_avg'] = df_windows[cols].mean(axis=1)    
+    df_windows['md_std'] = df_windows[cols].var(axis=1).map(sqrt)
+    return df_windows 
+
+def calc_avg(pdbid,parm):
+    "grab all dataframes avg them and output the outfile"
+    df_windows = glob_df(pdbid,parm,chainA=False)
+    df_windows = getavg(df_windows)
+    outfilename = '_'.join([pdbid,parm,'avg-dfianalysis.csv'])
+    df_windows.to_csv(outfile)
+    print "Wrote out to %s"%(outfilename)
